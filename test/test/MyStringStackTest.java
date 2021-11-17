e4b97d0f25d0db990b31c6f0fc98792fa3ce5c53
zackxz <limkm@live.com>
2021-11-17 11:14:07 +0800

    Fail Test Case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,9 +31,9 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
